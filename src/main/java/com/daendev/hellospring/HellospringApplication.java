f7490e30749a07990535b299d1cb67e7f3a4b4d2
daen <g.dani325@gmail.com>
2018-07-06 03:08:21 +0200

    Deployable to Tomcat

--- src/main/java/com/daendev/hellospring/HellospringApplication.java
+++ src/main/java/com/daendev/hellospring/HellospringApplication.java
@@ -2,10 +2,17 @@ package com.daendev.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HellospringApplication {
+public class HellospringApplication extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HellospringApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HellospringApplication.class, args);
 	}
